ac486fb1580b63d0b7f6129e15c4f385c242ef82
Prasiddh <61389919+prapooskur@users.noreply.github.com>
2025-09-11 13:31:36 -0700

    oops

--- backend/RAG/vectordb.py
+++ backend/RAG/vectordb.py
@@ -5,7 +5,9 @@ from llama_index.core.vector_stores import MetadataFilters, ExactMatchFilter
 
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
-from llama_index.embeddings.ollama import OllamaEmbedding, Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+
 import chromadb
 
 from typing import List
